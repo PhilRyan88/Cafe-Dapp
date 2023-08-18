ae415dc083e3c3ab92227341b0b6708086f2ea21
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-18 18:07:21 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,33 @@ function App() {
     signer: null,
     contract: null,
   });
+  const [account, setAccount] = useState("Not connected");
   useffect(() => {
-    const template = () => {
+    const template = async () => {
       const contractAddress = "";
       const contractABI = "";
+      //gotta use metamask here
+      try {
+        const { ethereum } = window;
+        const account = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(account);
+
+        const provider = new ethers.providers.Web3Provider(ethereum); //reading the blockchain
+        const signer = provider.getSigner(); //writting to the blockchain
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        setState({ provider, signer, contract });
+      } catch (error) {
+        alert(error);
+      }
     };
-  });
+    template();
+  }, []);
 
   return <div className="App"></div>;
 }
