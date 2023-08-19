f7fc52667da7088b4f4b972538b938e8837f42cc
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-19 19:56:20 +0530

    initialize

--- contracts/chai.sol
+++ contracts/chai.sol
@@ -1,28 +1,28 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-contract chai{
-
-    struct Memo{
+contract chai {
+    struct Memo {
         string name;
         string message;
-        uint timestamp;
+        uint256 timestamp;
         address from;
     }
 
     Memo[] memos;
-    address payable owner; //owner is going to receive funds
-    constructor(){
+    address payable owner;
+
+    constructor() {
         owner = payable(msg.sender);
     }
 
-    function buyChai(string calldata name,string calldata message) external payable{
-        require(msg.value>0,"Please pay more than 0 ether");
+    function buyChai(string memory name, string memory message) public payable {
+        require(msg.value > 0, "Please pay greater than 0 ether");
         owner.transfer(msg.value);
-        memos.push(Memo(name,message,block.timestamp,msg.sender));
+        memos.push(Memo(name, message, block.timestamp, msg.sender));
     }
 
-    function getMemos() public view returns(Memo[] memory){
+    function getMemos() public view returns (Memo[] memory) {
         return memos;
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,56 @@
 const hre = require("hardhat");
+async function getBalances(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
 
+async function cosoleBalances(addresses) {
+  let counter = 0;
+  for (const address of addresses) {
+    console.log(`Address ${counter} balance:`, await getBalances(address));
+    counter++;
+  }
+}
+async function consoleMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.from;
+    const message = memo.message;
+    console.log(
+      `At ${timestamp},name ${name},address ${from},message ${message}`
+    );
+  }
+}
 async function main() {
-  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
-  const chai = await Chai.deploy(); //creating an instance of our smart contract
+  const [owner, from1, from2, from3] = await hre.ethers.getSigners();
+  const chai = await hre.ethers.getContractFactory("chai");
+  const contract = await chai.deploy(); //instance of contract
 
-  await chai.deployed(); //deploying your smart contract
+  await contract.deployed();
+  console.log("Address of contract:", contract.address);
 
-  console.log("Deployed contract address:", `${chai.address}`);
+  const addresses = [
+    owner.address,
+    from1.address,
+    from2.address,
+    from3.address,
+  ];
+  console.log("Before buying chai");
+  await cosoleBalances(addresses);
+
+  const amount = { value: hre.ethers.utils.parseEther("1") };
+  await contract.connect(from1).buyChai("from1", "Very nice chai", amount);
+  await contract.connect(from2).buyChai("from2", "Very nice course", amount);
+  await contract
+    .connect(from3)
+    .buyChai("from3", "Very nice information", amount);
+
+  console.log("After buying chai");
+  await cosoleBalances(addresses);
+
+  const memos = await contract.getMemos();
+  consoleMemos(memos);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
