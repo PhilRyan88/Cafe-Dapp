7bdfe164128dbef0fdd4d0da6a83895c89a9ce36
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-08-21 22:11:30 +0530

    initialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import abi from "./contracts/Cafe.json";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0xCf9B90AbeCF7C31Bf2dd32f1A4B390654a970319";
+      const contractAbi = abi.abi;
+      try{
+        const {ethereum}= window;
+      }
+
+    };
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
